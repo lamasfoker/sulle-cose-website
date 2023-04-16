cdc3d25c86bc842e5c9c5a4910b0ba6162d8524a
Giacomo Moscardini <gmoscardini@webgriffe.com>
2023-04-16 14:50:34 +0200

    Commit manifest.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
 const WorkboxPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
@@ -12,26 +11,6 @@ module.exports = {
         path: path.resolve(__dirname, 'assets/build/'),
     },
     plugins: [
-        new WebpackPwaManifest({
-            inject: false,
-            fingerprints: false,
-            filename: 'manifest.json',
-            name: 'Sulle Cose',
-            short_name: 'Sulle Cose',
-            background_color: '#ffffff',
-            theme_color: '#f3f4f6',
-            dir: 'ltr',
-            orientation: 'any',
-            prefer_related_applications: false,
-            publicPath: '/' ,
-            icons: [
-                {
-                    src: './assets/favicon.png',
-                    sizes: [192, 180, 167, 152, 144, 120, 114, 96, 76, 72, 60, 57, 32, 16],
-                    destination: 'icon',
-                },
-            ],
-        }),
         new WorkboxPlugin.GenerateSW({
             // these options encourage the ServiceWorkers to get in there fast
             // and not allow any straggling "old" SWs to hang around
